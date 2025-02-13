ee50750ddd2c666426bc3654f499e5285e942e17
Samriddha9619 <sumitkumartripathi0@gmail.com>
2025-02-13 11:34:15 +0530

    commit

--- YT_downloader/myapp/downloader/views.py
+++ YT_downloader/myapp/downloader/views.py
@@ -1,23 +1,22 @@
-from django.shortcuts import render, redirect 
-from pytube import *
-  
-  
-# defining function 
-def youtube(request): 
-  
-    # checking whether request.method is post or not 
-    if request.method == 'POST': 
+from django.shortcuts import render
+import yt_dlp
+def youtube_downloader(request):
+    if request.method == "POST":
+        video_url = request.POST.get("video_url")
+        try:
+            # yt-dlp options
+            ydl_opts = {
+                'format': 'best',  # Get best available quality
+            }
+
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(video_url, download=False)  # Get video info without downloading
+                download_url = info.get("url")  # Get direct video URL
+                video_title = info.get("title", "Unknown Title")
+
+            return render(request, "downloader/youtube.html", {"download_url": download_url, "video_title": video_title})
         
-        # getting link from frontend 
-        link = request.POST['link'] 
-        video = YouTube(link) 
-  
-        # setting video resolution 
-        stream = video.streams.get_lowest_resolution() 
-          
-        # downloads video 
-        stream.download() 
-  
-        # returning HTML page 
-        return render(request, 'downloader/youtube.html') 
-    return render(request, 'downloader/youtube.html')
+        except Exception as e:
+            return render(request, "downloader/youtube.html", {"error": "Invalid URL or video cannot be downloaded."})
+
+    return render(request, "downloader/youtube.html")
--- YT_downloader/myapp/myapp/urls.py
+++ YT_downloader/myapp/myapp/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include,path
 from downloader import views
 urlpatterns = [
-    path('', views.youtube, name='youtube'),
+    
     path('admin/', admin.site.urls),   
+    path('', include('downloader.urls')),
 ]
